a170fe065aa97e07882f85f2d6562e37791a495d
kvn29 <kevin@pc15.home>
2016-12-21 21:28:43 +0100

    Checkbox

--- data/categoryData.js
+++ data/categoryData.js
@@ -1,18 +1,33 @@
 
 const tab = {
   "category":[
-    "Multimédia",
-    "Loisirs",
-    "Vêtements",
-    "Chaussures",
-    "Equipement Bébé",
-    "Décoration",
-    "Vintage",
-    "Collections",
-    "Electroménager",
-    "Ameublement",
-    "Vaisselle",
-    "Bricolage"
+    {
+      name: "Multimédia",
+      checked: true
+    },
+    {
+      name: "Mobilier",
+      checked: false
+    }
   ]
 }
 module.exports = tab;
+
+//
+// const tab = {
+//   "category":[
+//     "Multimédia",
+//     "Loisirs",
+//     "Vêtements",
+//     "Chaussures",
+//     "Equipement Bébé",
+//     "Décoration",
+//     "Vintage",
+//     "Collections",
+//     "Electroménager",
+//     "Ameublement",
+//     "Vaisselle",
+//     "Bricolage"
+//   ]
+// }
+// module.exports = tab;
--- ui/categories.js
+++ ui/categories.js
@@ -19,52 +19,88 @@ import {
   Button
   } from 'native-base';
 
-  import { Actions } from 'react-native-router-flux';
-  var test = require('../data/categoryData.js');
+import { Actions } from 'react-native-router-flux';
+var test = require('../data/categoryData.js');
 
+import CheckBoxCustom from '../ui/checkbox.js'
 
-  class categories extends Component {
+class categories extends Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        typeList: props.navigationState.typeList,
-        checkbox: false,
-        radio: false,
-        category: test.category
-      };
-
-    }
-
-  toggleSwitch() {
-    this.setState({
-      checkbox: !this.state.checkbox
-    });
-  }
-
-  toggleRadio() {
-    this.setState({
-      radio: !this.state.radio
-    })
+  constructor(props) {
+    super(props);
+    this.state = {
+      typeList: props.navigationState.typeList,
+      category: test.category
+    };
   }
 
   componentWillMount() {
     this.forceUpdate();
   }
+  toggleSwitch(name) {
+    console.log(this.state.category);
+
+    var clone = this.state.category.slice(0);
+    for(var item in clone) {
+      // console.log(clone[item]);
+      if(clone[item].name === name) {
+        console.log("AVANT : ", clone[item].checked);
+        clone[item].checked = !clone[item].checked;
+        console.log("APRES : ", clone[item].checked);
+      }
+    }
+    clone.push('te');
+
+    this.setState({
+      category: clone
+    }, () => {
+      this.forceUpdate();
+      console.log(this.state.category);
+      this.render();
+      Actions.pop();
+    });
+    // this.forceUpdate();
+    // this.render();
+  }
+  // componentWillMount() {
+  //   setTimeout(() => {
+  //     console.log('aa')
+  //     this.forceUpdate();
+  //   }, 5000);
+  // }
+
+
+  // <List dataArray={this.state.category} renderRow={(item)=>
+  //   <ListItem button onPress={this.toggleSwitch.bind(this, item.name)}>
+  //     <CheckBoxCustom active={item.checked} />
+  //     <Text>{item.name}</Text>
+  //   </ListItem>
+  //  }>
+  //   </List>
+  // Ce qu'on veut faire : Lors du clic sur listitem, on coche la case interne.
+  // En passant a la checkbox componont le nom de la fonction parent l'enfant peut déclencher une méthode du parent
 
     render() {
       if(this.state.typeList == "checkbox") {
         return (
-          <View style={{marginTop:60}} >
-            <List dataArray={this.state.category} renderRow={(item)=>
-              <ListItem button onPress={this.toggleSwitch.bind(this)}>
-                <CheckBox checked={this.state.checkbox === true} />
-                <Text>{item}</Text>
-              </ListItem>
-                }>
+          <View style={{marginTop:60}}>
+            <List>
+            {
+
+              this.state.category.map((item, i) => {
+                if(item.name) {
+                  console.log('RENDU');
+                return (
+                  <ListItem key={i} button onPress={this.toggleSwitch.bind(this, item.name)}>
+                    <CheckBoxCustom active={item.checked} />
+                    <Text>aa</Text>
+                  </ListItem>)
+                }
+              })
+            }
             </List>
-        <Button style={{marginTop:5, alignSelf: 'center'}}>Valider</Button>
-        </View>
+           <Button style={{marginTop:5, alignSelf: 'center'}}>Valider</Button>
+         </View>
         )
       }
       else {
@@ -77,7 +113,6 @@ import {
               </ListItem>
               }>
             </List>
-          <Button style={{marginTop:5, alignSelf: 'center'}}>Valider</Button>
           </View>
         );
       }
